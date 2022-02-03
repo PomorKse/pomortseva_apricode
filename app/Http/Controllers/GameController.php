a40b9a79cf002b119dc42f209c8868e04d63407e
Kseniia <kseniia.pomortseva@gmail.com>
2022-02-04 00:50:41 +0500

    controllersAndRequests

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateGameRequest;
+use App\Http\Requests\UpdateGameRequest;
 
 class GameController extends Controller
 {
-    //
+    public function index(Game $games)
+    {
+        $games = $games->all();
+
+        return response()->json(['games' => $games]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateGameRequest $request)
+    {
+        $game = Game::create($request->validated());
+        
+        if($game) {
+            return response()->json(['message' => 'Game created successfully']);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Game $game)
+    {
+        $game = $game->find($game);
+
+        return response()->json(['game' => $game]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGameRequest $request, Game $game)
+    {
+        $game = $game->fill($request->validated())->save();
+       
+        if($game){
+            return response()->json(['message' => 'Game updated successfully']);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Game $game)
+    {
+        try{
+			$game->delete();
+
+			return response()->json([
+                'success' => true,
+                'message' => 'Game deleted successfully'
+            ]);
+		}catch (\Exception $e) {
+			\Log::error($e->getMessage() . PHP_EOL, $e->getTrace());
+
+			return response()->json(['success' => false]);
+		}
+    }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateGenreRequest;
+use App\Http\Requests\UpdateGenreRequest;
 use Illuminate\Http\Request;
+use App\Models\Genre;
 
 class GenreController extends Controller
 {
-    //
+    public function index(Genre $genres)
+    {
+        $genres = $genres->all();
+
+        return response()->json(['genres' => $genres]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateGenreRequest $request)
+    {
+        $genre = Genre::create($request->validated());
+        
+        if($genre) {
+            return response()->json(['message' => 'Genre created successfully']);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Genre $genre)
+    {
+        $genre = $genre->find($genre);
+
+        return response()->json(['genre' => $genre]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGenreRequest $request, Genre $genre)
+    {
+        $genre = $genre->fill($request->validated())->save();
+       
+        if($genre){
+            return response()->json(['message' => 'Genre updated successfully']);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Genre $genre)
+    {
+        try{
+			$genre->delete();
+
+			return response()->json([
+                'success' => true,
+                'message' => 'Genre deleted successfully'
+            ]);
+		}catch (\Exception $e) {
+			\Log::error($e->getMessage() . PHP_EOL, $e->getTrace());
+
+			return response()->json(['success' => false]);
+		}
+    }
 }
